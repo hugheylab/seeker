2a0e7ee9cb288133efee8a4b05b928dbd222a5c6
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2021-11-16 14:21:27 -0600

    Use 2 separate directories for testing

--- tests/testthat/test_seeker.R
+++ tests/testthat/test_seeker.R
@@ -3,6 +3,7 @@ params = yaml::read_yaml('test_data/GSE143524.yml')
 params$fetch$run = FALSE
 foreach::registerDoSEQ()
 parentDir = 'test_data/staging'
+parentDir2 = 'test_data/staging2'
 
 test_that('Test seeker', {
   dir.create(parentDir)
@@ -14,9 +15,9 @@ test_that('Test seeker', {
 })
 
 test_that('Test checkSeekerArgs', {
-  dir.create(parentDir)
-  withr::local_file(parentDir)
-  file.copy('test_data/GSE143524', parentDir, recursive = TRUE)
-  outputDirObs = seeker:::checkSeekerArgs(params, parentDir)
-  expect_equal(outputDirObs, file.path(parentDir, 'GSE143524'))
+  dir.create(parentDir2)
+  withr::local_file(parentDir2)
+  file.copy('test_data/GSE143524', parentDir2, recursive = TRUE)
+  outputDirObs = seeker:::checkSeekerArgs(params, parentDir2)
+  expect_equal(outputDirObs, file.path(parentDir2, 'GSE143524'))
 })
