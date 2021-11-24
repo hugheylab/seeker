82de2e49e8205817e495d84ebfe1239c090dc4fe
Jake Hughey <jakejhughey@gmail.com>
2021-11-23 19:39:04 -0600

    added vignette for reproducibility. cleaned up example files.

--- inst/extdata/run_seeker.sh
+++ inst/extdata/run_seeker.sh
@@ -1,20 +1,7 @@
-##########
-# if refgenie genomes folder and salmon index available on host
+#!/bin/sh
 
 docker run \
-  --mount type=bind,src=/Users/jakehughey/projects,dst=/home/rstudio/projects \
-  --mount type=bind,src=/Users/jakehughey/genomes,dst=/home/rstudio/genomes \
-  -w /home/rstudio/projects \
-  --rm \
-  ghcr.io/hugheylab/socker \
-  bash -c "Rscript run_seeker.R GSE113883.yml ." \
-  &> progress.log
-
-##########
-# if refgenie genomes folder not available on host
-
-docker run \
-  --mount type=bind,src=/Users/jakehughey/projects,dst=/home/rstudio/projects \
+  --mount type=bind,src=`pwd`,dst=/home/rstudio/projects \
   -w /home/rstudio/projects \
   --rm \
   ghcr.io/hugheylab/socker \
@@ -22,5 +9,5 @@ docker run \
     "source /home/rstudio/miniconda3/etc/profile.d/conda.sh \
       && conda activate \
       && refgenie pull mm10/salmon_partial_sa_index \
-      && Rscript run_seeker.R GSE143524.yml ." \
-  &> progress.log
+      && Rscript run_seeker.R PRJNA600892.yml ." \
+  &> PRJNA600892_progress.log
