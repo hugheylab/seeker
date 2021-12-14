7bdb97c4890ee5c57c6ce8833da037443b4fe62d
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2021-12-14 11:22:27 -0600

    Expect list.files equal in seeker run

--- tests/testthat/test-seeker.R
+++ tests/testthat/test-seeker.R
@@ -1,8 +1,19 @@
 test_that('Test seeker', {
   skip_on_os('windows', arch = NULL)
-  seeker(params, parentDir)
-  expect_true(file.exists(file.path(parentDir, 'GSE143524', 'salmon_meta_info.csv')))
-  expect_equal(2L, nrow(fread(file.path(parentDir, 'GSE143524', 'salmon_meta_info.csv'))))
+
+  # Add unique folders for seeker full output
+  parentDirSeeker = 'test_data/staging_seeker'
+  dir.create(parentDirSeeker)
+  withr::local_file(parentDirSeeker)
+  file.copy('test_data/GSE143524', parentDirSeeker, recursive = TRUE)
+  outputDirSeeker = file.path(parentDirSeeker, 'GSE143524')
+
+  seeker(params, parentDirSeeker)
+
+  seekerOutputObs = sort(list.files(outputDirSeeker, recursive = TRUE))
+  seekerOutputControl = sort(readRDS('test_data/seeker_output_full.rds'))
+
+  expect_equal(seekerOutputObs, seekerOutputControl)
 })
 
 test_that('Test checkSeekerArgs', {
