091c210bd28fd67305b7ef2106441ac2c2c933a8
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2021-12-14 11:33:08 -0600

    Add seeker skip all test function

--- tests/testthat/test-seeker.R
+++ tests/testthat/test-seeker.R
@@ -16,6 +16,25 @@ test_that('Test seeker', {
   expect_equal(seekerOutputObs, seekerOutputControl)
 })
 
+test_that('Test seeker skip all', {
+  skip_on_os('windows', arch = NULL)
+
+  # Add unique folders for seeker skipped output
+  paramsSkip = yaml::read_yaml('test_data/GSE143524_skip_all.yml')
+  parentDirSeekerSkip = 'test_data/staging_seeker_skip'
+  dir.create(parentDirSeekerSkip)
+  withr::local_file(parentDirSeekerSkip)
+  file.copy('test_data/GSE143524', parentDirSeekerSkip, recursive = TRUE)
+  outputDirSeekerSkip = file.path(parentDirSeekerSkip, 'GSE143524')
+
+  seeker(paramsSkip, parentDirSeekerSkip)
+
+  seekerOutputObs = sort(list.files(outputDirSeekerSkip, recursive = TRUE))
+  seekerOutputControl = sort(readRDS('test_data/seeker_output_skip.rds'))
+
+  expect_equal(seekerOutputObs, seekerOutputControl)
+})
+
 test_that('Test checkSeekerArgs', {
   skip_on_os('windows', arch = NULL)
   outputDirObs = seeker:::checkSeekerArgs(params, parentDir)
