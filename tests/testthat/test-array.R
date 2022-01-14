1b906721f509cc756b262e9f2b26eb155376a645
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2022-01-14 11:01:32 -0600

    Use different array dataset for local, shorten variable names

--- tests/testthat/test-array.R
+++ tests/testthat/test-array.R
@@ -1,16 +1,16 @@
 paramsArray = yaml::read_yaml(file.path(dataDir, 'GSE25585.yml'))
-parentDirSeekerArray = file.path(dataDir, 'staging_seeker_array')
-dir.create(parentDirSeekerArray)
-withr::local_file(parentDirSeekerArray)
+parentDirArr = file.path(dataDir, 'staging_seeker_array')
+dir.create(parentDirArr)
+withr::local_file(parentDirArr)
 
 test_that('checkSeekerArrayArgs', {
   skip_on_os('windows', arch = NULL)
 
-  checkSeekerArrayArgsObs = checkSeekerArrayArgs(paramsArray, parentDirSeekerArray)
+  resultObs = checkSeekerArrayArgs(paramsArray, parentDirArr)
 
-  checkSeekerArrayArgsExp = snapshot(checkSeekerArrayArgsObs, file.path(dataDir, 'seeker_array_args_output.qs'))
+  resultExp = snapshot(resultObs, file.path(dataDir, 'seeker_array_args_output.qs'))
 
-  expect_equal(checkSeekerArrayArgsObs, checkSeekerArrayArgsExp)
+  expect_equal(resultObs, resultExp)
 })
 
 test_that('checkSeekerArrayArgs errors', {
@@ -20,60 +20,60 @@ test_that('checkSeekerArrayArgs errors', {
 
   # GSE platform not null or GPL
   paramsArrayErr$platform = 'abcd'
-  expect_error(checkSeekerArrayArgs(paramsArrayErr, parentDirSeekerArray))
+  expect_error(checkSeekerArrayArgs(paramsArrayErr, parentDirArr))
 
   # E- with platform
   paramsArrayErr$study = 'E-test'
-  expect_error(checkSeekerArrayArgs(paramsArrayErr, parentDirSeekerArray))
+  expect_error(checkSeekerArrayArgs(paramsArrayErr, parentDirArr))
 
   # raw with raw dir not existing
   paramsArrayErr$study = 'LOCAL'
   paramsArrayErr$platform = 'GPL1'
-  expect_error(checkSeekerArrayArgs(paramsArrayErr, parentDirSeekerArray))
+  expect_error(checkSeekerArrayArgs(paramsArrayErr, parentDirArr))
 })
 
 test_that('seekerArray GSE', {
   skip_on_os('windows', arch = NULL)
 
-  seekerArray(paramsArray, parentDirSeekerArray)
+  seekerArray(paramsArray, parentDirArr)
 
-  seekerArrayOutputObs = list.files(parentDirSeekerArray, recursive = TRUE)
-  seekerArrayOutputExp = snapshot(seekerArrayOutputObs, file.path(dataDir, 'seeker_array_output_full.qs'))
+  resultObs = list.files(parentDirArr, recursive = TRUE)
+  resultExp = snapshot(resultObs, file.path(dataDir, 'seeker_array_output_full.qs'))
 
-  expect_equal(seekerArrayOutputObs, seekerArrayOutputExp)
+  expect_equal(resultObs, resultExp)
 })
 
 test_that('seekerArray Ae', {
   skip_on_os('windows', arch = NULL)
 
-  parentDirSeekerArrayAe = file.path(dataDir, 'staging_seeker_array_ae')
-  dir.create(parentDirSeekerArrayAe)
-  withr::local_file(parentDirSeekerArrayAe)
+  parentDirArrAe = file.path(dataDir, 'staging_seeker_array_ae')
+  dir.create(parentDirArrAe)
+  withr::local_file(parentDirArrAe)
   paramsArrayAe = paramsArray
   paramsArrayAe$study = 'E-MTAB-8714'
 
-  seekerArray(paramsArrayAe, parentDirSeekerArrayAe)
+  seekerArray(paramsArrayAe, parentDirArrAe)
 
-  seekerArrayAeOutputObs = list.files(parentDirSeekerArrayAe, recursive = TRUE)
-  seekerArrayAeOutputExp = snapshot(seekerArrayAeOutputObs, file.path(dataDir, 'seeker_array_ae_output_full.qs'))
+  resultObs = list.files(parentDirArrAe, recursive = TRUE)
+  resultExp = snapshot(resultObs, file.path(dataDir, 'seeker_array_ae_output_full.qs'))
 
-  expect_equal(seekerArrayAeOutputObs, seekerArrayAeOutputExp)
+  expect_equal(resultObs, resultExp)
 })
 
 test_that('seekerArray LOCAL', {
   skip_on_os('windows', arch = NULL)
 
-  parentDirSeekerArrayLocal = file.path(dataDir, 'staging_seeker_array_local')
-  dir.create(parentDirSeekerArrayLocal)
-  withr::local_file(parentDirSeekerArrayLocal)
+  parentDirArrLcl = file.path(dataDir, 'staging_seeker_array_local')
+  dir.create(parentDirArrLcl)
+  withr::local_file(parentDirArrLcl)
   paramsArrayLocal = yaml::read_yaml(file.path(dataDir, 'LOCAL01.yml'))
-  file.copy(file.path(dataDir, 'LOCAL01'), parentDirSeekerArrayLocal, recursive = TRUE)
+  file.copy(file.path(dataDir, 'LOCAL01'), parentDirArrLcl, recursive = TRUE)
 
-  seekerArray(paramsArrayLocal, parentDirSeekerArrayLocal)
+  seekerArray(paramsArrayLocal, parentDirArrLcl)
 
-  seekerArrayLocalOutputObs = list.files(parentDirSeekerArrayLocal, recursive = TRUE)
-  seekerArrayLocalOutputExp = snapshot(seekerArrayLocalOutputObs, file.path(dataDir, 'seeker_array_local_output_full.qs'))
+  resultObs = list.files(parentDirArrLcl, recursive = TRUE)
+  resultExp = snapshot(resultObs, file.path(dataDir, 'seeker_array_local_output_full.qs'))
 
-  expect_equal(seekerArrayLocalOutputObs, seekerArrayLocalOutputExp)
+  expect_equal(resultObs, resultExp)
 })
 
