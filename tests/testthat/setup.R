44c0bbcd77ecf7ead710eed24c5a974e910b4efc
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2022-01-27 10:18:00 -0600

    Skip steps if commands are missing

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -10,6 +10,29 @@ snapshot = function(xObs, path) {
     xExp = xObs}
   return(xExp)}
 
+commandsExists = function(params, cmds = seeker:::checkDefaultCommands()) {
+  exists = foreach(i = 1:nrow(cmds), .combine = rbind) %do% {
+    cmdRow = cmds[i,]
+    cmd = if (cmdRow$command == 'ascp') {
+      params$fetch$ascpCmd
+    } else if (cmdRow$command == 'trim_galore') {
+      params$trimgalore$cmd
+    } else if (cmdRow$command %in% names(params)) {
+      params[[cmdRow$command]]$cmd
+    } else {
+      NULL}
+    cmdExists = TRUE
+    if (is.null(cmd)) {
+      if (is.na(cmdRow$path)) {
+        cmdExists = FALSE}
+    } else {
+      path = checkCommand(cmd)
+      if (is.na(path)) {
+        cmdExists = FALSE}}
+    cmdRow[, exists := cmdExists]
+  }
+  return(exists)}
+
 registerDoSEQ()
 dataDir = 'data'
 params = yaml::read_yaml(file.path(dataDir, 'GSE143524.yml'))
@@ -21,6 +44,10 @@ if (Sys.info()['user'] != 'runner') {
   params$salmon$indexDir = gsub('/runner/',
                                 paste0('/', Sys.info()['user'], '/'),
                                 params$salmon$indexDir)}
+
+missingCommands = rbind(commandsExists(params), data.table(command = 'samlmon_index', path = params$salmon$indexDir, exists = file.exists(params$salmon$indexDir)), fill = TRUE)
+anyMissing = any(!missingCommands[['exists']])
+
 
 params$fetch$run = FALSE
 parentDir = file.path(dataDir, 'staging')
--- tests/testthat/test-salmon.R
+++ tests/testthat/test-salmon.R
@@ -1,4 +1,5 @@
 test_that('salmon and getSalmonMetadata', {
+  skip_if((!missingCommands[command == 'salmon',]$exists | !missingCommands[command == 'salmon_index',]$exists), 'Missing salmon command and/or index, skipping.')
   skip_on_os('windows', arch = NULL)
   step = 'salmon'
   paramsNow = params[[step]]
