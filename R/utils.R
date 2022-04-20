598e49331742062a6bfec704aedcb04b3afdcd32
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2022-04-20 11:16:59 -0500

    Add env to mamba install

--- R/utils.R
+++ R/utils.R
@@ -373,7 +373,7 @@ installTools = function(sraToolkitPath = '~', sraAddToPath = TRUE,
                   'refgenie',
                   'salmon',
                   'trim-galore')
-    system3('conda', c('install', '--yes', 'mamba', '-c', 'conda-forge'))
+    system3('conda', c('install', 'mamba', '--yes', '-n', minicondaEnv, '-c', 'conda-forge'))
     system3('mamba', c('install', '--yes', mambaPkgs))
   }
 
