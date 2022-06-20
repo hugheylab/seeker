3f1d75f8f7a9b9722e02a40871720ab54e45a760
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2022-06-20 09:49:10 -0500

    Update tests to check for gene mapping version

--- tests/testthat/test-array-utils.R
+++ tests/testthat/test-array-utils.R
@@ -162,6 +162,10 @@ test_that('getEntrezEnsemblMapping', {
 
 
 test_that('getProbeGeneMapping', {
+  mapPkgVerObs = packageVersion('org.Mm.eg.db')
+  mapPkgVerExp = snapshot(mapPkgVerObs, file.path(dataDir, 'map_pkg_ver.qs'))
+  skip_if(mapPkgVerObs != mapPkgVerExp, 'Mapping package is different version, skipping.')
+
   platforms = getPlatforms('mapping')
   annos = c('GPL6887', 'GPL7202')
 
