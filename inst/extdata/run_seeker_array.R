1909c98e54db29e259f940c735607222a5843be4
Josh Schoenbachler <josh.schoenbachler@gmail.com>
2022-06-21 16:23:45 -0500

    Update vignette and script to accept yaml

--- inst/extdata/run_seeker_array.R
+++ inst/extdata/run_seeker_array.R
@@ -1,9 +1,18 @@
 cArgs = commandArgs(TRUE)
-study = cArgs[1L]
-geneIdType = cArgs[2L]
-platform = NULL
-parentDir = cArgs[3L]
-if (length(cArgs) > 3) {
+if (length(cArgs) == 2) {
+  params = yaml::read_yaml(cArgs[1L])
+  study = params$study
+  geneIdType = params$geneIdType
+  platform = params$platform
+  parentDir = cArgs[2L]
+} else if (length(cArgs) == 3) {
+  study = cArgs[1L]
+  geneIdType = cArgs[2L]
+  platform = NULL
+  parentDir = cArgs[3L]
+} else {
+  study = cArgs[1L]
+  geneIdType = cArgs[2L]
   platform = cArgs[3L]
   parentDir = cArgs[4L]
 }
